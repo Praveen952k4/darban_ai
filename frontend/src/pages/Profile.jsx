1a298f2dc10e48e4223eb8b4483fd07005857603
Praveen952k4 <praveenkalimuthu@gmail.com>
2025-05-23 09:34:40 +0530

    commit

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import Navbar from '../components/Navbar';
-import Bottombar from '../components/Bottombar';
-import Profile from '../components/Profile';
-import SideBar from '../components/SideBar';
 
 const Help = () => {
   return (
      <div className="min-h-screen w-full bg-black text-white flex flex-col">
-      <Navbar />
-      <div className="flex flex-1 overflow-hidden">
-        <SideBar />
-        <div className="flex-1 p-4 overflow-y-auto h-full">
-          <Profile />
-        </div>
-      </div>
-      <Bottombar />
     </div>
   );
 }
