09f79185a81883e86b64fde5cbea1756ec40ebee
Praveen952k4 <praveenkalimuthu@gmail.com>
2025-05-23 09:33:58 +0530

    message

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Bottombar from '../components/Bottombar';
-import SideBar from '../components/SideBar';
 import Profile from '../components/Profile';
+import SideBar from '../components/SideBar';
 
 const Help = () => {
   return (
      <div className="min-h-screen w-full bg-black text-white flex flex-col">
       <Navbar />
       <div className="flex flex-1 overflow-hidden">
-        <SideBar/>
+        <SideBar />
         <div className="flex-1 p-4 overflow-y-auto h-full">
           <Profile />
         </div>
